6dfe8ab5438d8d5f56d2c942dddc1b8050ce5ff8
Andrew Paterson <andrew.paterson9@gmail.com>
2026-03-27 23:48:34 +0200

    Docs tests

--- test-app/src/components/ReactLive.jsx
+++ test-app/src/components/ReactLive.jsx
@@ -1,12 +1,13 @@
 import DocsDemo from './docs-utils/DocsDemo';
 import DocsSnippet from './docs-utils/DocsDemo';
-
+import AttrFunctions from './demos/AttrFunctions';
 export default function CodeDemo() {
   return (
     <>
-      <DocsSnippet snippet={`{"foo": "bars"}`} />
+      {/* <DocsSnippet snippet={`{"foo": "bars"}`} />
       <div>Test</div>
-      {<DocsDemo snippet={`<button onClick={() => alert('Helzzlo!')}>Click me</button>`} />}
+      <DocsDemo snippet={`<button onClick={() => alert('Helzzlo!')}>Click me</button>`} /> */}
+      <AttrFunctions />
     </>
   );
 }
--- test-app/src/components/docs-utils/DocsDemo.jsx
+++ test-app/src/components/docs-utils/DocsDemo.jsx
@@ -1,8 +1,11 @@
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
-export default function DocsDemo({ snippet }) {
+export default function DocsDemo({ snippet, scope }) {
   return (
-    <LiveProvider code={snippet}>
+    <LiveProvider
+      code={snippet}
+      scope={scope}
+    >
       <LiveEditor /> {/* editable code box */}
       <LiveError /> {/* shows syntax/runtime errors */}
       <LivePreview /> {/* live rendered output */}
